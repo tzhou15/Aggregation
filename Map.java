959e2d9353ca4b5bba9ac4b99b091da701440fe4
tzhou15 <68517166+tzhou15@users.noreply.github.com>
2020-07-26 00:37:37 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -6,12 +6,16 @@ public class Map extends Mapper<Object, Text, Text, Text> {
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		String[] tokens = value.toString().split(",");
 		String time = tokens[0].replaceAll("\\:|\\.|", "");
-		String hourData = time.substring(0, 2);
+		String hourData = time.substring(8,9);
+		String minData = time.substring(10,11);
+		String secData = time.substring(12,13);
 		int hour = Integer.parseInt(hourData);
-		String Key = time.trim() + "\t" + tokens[1].trim();
+		int min = Integer.parseInt(minData);
+		int sec = Integer.parseInt(secData);
+		String Key = hour + "\t" + min + "\t" + sec + "\t" + tokens[1].trim();
 		String Value = tokens[2];
 		int i;
-		for(i=3; i<68; i++) {
+		for(i=3; i<tokens.length; i++) {
 			Value = Value + "\t" + tokens[i];
 		}
 		context.write(new Text(Key), new Text(Value));
