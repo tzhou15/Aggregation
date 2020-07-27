4b2bbf0ec051d8c8b052b7042873a683f58582f0
tzhou15 <68517166+tzhou15@users.noreply.github.com>
2020-07-26 22:57:39 -0700

    Update Reduce.java

--- Reduce.java
+++ Reduce.java
@@ -1,4 +1,30 @@
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class Reduce {
-
+public class Reduce extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+	    double min = Integer.MAX_VALUE, max = 0;
+	    Iterator<DoubleWritable> iterator = values.iterator();
+	    double count = 0;
+	    double sum = 0;
+	    while (iterator.hasNext()) {
+	    	double value = iterator.next().get();
+		    sum += iterator.next().get();
+		    count++;
+		    if(value < min) { 
+			    min = value;
+		    }
+		    if(value > max) { 
+			    max = value;
+		    }
+	    }
+	    double average = (int) sum/count;
+	    context.write(new Text(key), new DoubleWritable(min));
+	    context.write(new Text(key), new DoubleWritable(max));
+	    context.write(new Text(key), new DoubleWritable(average));
+	    context.write(new Text(key), new DoubleWritable(count));
+	   }
 }
