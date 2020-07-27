3e073750f9a3067eacc69823f668d245d977cc25
tzhou15 <68517166+tzhou15@users.noreply.github.com>
2020-07-26 22:56:34 -0700

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,8 +1,9 @@
 import java.io.IOException;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class Map extends Mapper<Object, Text, Text, Text> {
+public class Map extends Mapper<Object, Text, Text, DoubleWritable> {
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		String[] tokens = value.toString().split(",");
 		String time = tokens[0].replaceAll("\\:|\\.|", "");
@@ -12,12 +13,11 @@ public class Map extends Mapper<Object, Text, Text, Text> {
 		int hour = Integer.parseInt(hourData);
 		int min = Integer.parseInt(minData);
 		int sec = Integer.parseInt(secData);
-		String Key = hour + "\t" + min + "\t" + sec + "\t" + tokens[1].trim();
-		String Value = tokens[2];
+		int id = Integer.parseInt(tokens[1]);
+		String Key = hour + "" + "" + min + "" + sec + "" + id;
 		int i;
-		for(i=3; i<tokens.length; i++) {
-			Value = Value + "\t" + tokens[i];
+		for(i=2; i<tokens.length; i++) {
+			context.write(new Text(Key + "" + String.valueOf("var00"+(i+1))), new DoubleWritable(Double.parseDouble(tokens[i])));
 		}
-		context.write(new Text(Key), new Text(Value));
 	}
 }
